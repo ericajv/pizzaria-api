3ed3eaac59e38e586f38ea363b0e741bc87aa0dc
Érica Vieira <ericaj.vieira13@gmail.com>
2024-04-30 11:21:38 -0300

    feat: Initial commit

---
+++ pizzaria-api/src/test/java/com/pizzariamineira/pizzariaapi/PizzariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzariamineira.pizzariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
